c58972daafbc501744ad8b619fd640f1ae7d3fc4
Tracy Scott <tracy.scott@gmail.com>
2023-06-16 17:40:04 -0700

    Joint visualization
    Added functionality to RadialSel pattern to visual joints at radials.

--- src/main/java/art/lookingup/spidertrap/patterns/RadialSel.java
+++ src/main/java/art/lookingup/spidertrap/patterns/RadialSel.java
@@ -1,5 +1,7 @@
 package art.lookingup.spidertrap.patterns;
 
+import art.lookingup.linear.Edge;
+import art.lookingup.linear.Joint;
 import art.lookingup.spidertrap.SpiderTrapModel;
 import art.lookingup.spidertrap.ui.ModelParams;
 import heronarts.lx.LX;
@@ -13,12 +15,14 @@ public class RadialSel extends LXPattern {
 
   CompoundParameter radialNum = new CompoundParameter("radial", -1, -1, SpiderTrapModel.MAX_RADIALS - 0.75).setDescription("radial");
   DiscreteParameter edgeSel = new DiscreteParameter("edge", -1, -1, 20);
+  DiscreteParameter jointSel = new DiscreteParameter("joint", -1, 0, 3);
   DiscreteParameter webNum = new DiscreteParameter("web", -1, -1, SpiderTrapModel.NUM_WEBS).setDescription("web");
 
   public RadialSel(LX lx) {
     super(lx);
     addParameter("radial", radialNum);
     addParameter("edge", edgeSel);
+    addParameter("joint", jointSel);
     addParameter("web", webNum);
   }
 
@@ -39,10 +43,27 @@ public class RadialSel extends LXPattern {
           int numRadEdges = radial.edges.size();
           for (int edgeN = 0; edgeN < numRadEdges; edgeN++) {
             if (radN == (int) Math.round(whichRadial) || (int) Math.round(whichRadial) == -1) {
-              if (edgeN == edgeSel.getValuei() || edgeSel.getValuei() == -1)
+              if (edgeN == edgeSel.getValuei() || edgeSel.getValuei() == -1) {
                 for (LXPoint p : radial.edges.get(edgeN).points) {
                   colors[p.index] = LXColor.WHITE;
                 }
+                int joint = jointSel.getValuei();
+                if (joint != -1 && edgeSel.getValuei() != -1) {
+                  Edge edge = SpiderTrapModel.allEdges.get(radial.edges.get(edgeN).id);
+                  Joint startJoint = edge.myStartPointJoints[joint];
+                  Joint endJoint = edge.myEndPointJoints[joint];
+                  if (startJoint != null) {
+                    for (LXPoint p : startJoint.edge.points) {
+                      colors[p.index] = LXColor.RED;
+                    }
+                  }
+                  if (endJoint != null) {
+                    for (LXPoint p : endJoint.edge.points) {
+                      colors[p.index] = LXColor.GREEN;
+                    }
+                  }
+                }
+              }
             }
           }
         }
